e037b0df27a4f62e77bc78d5bb558f78841a3a1b
fmarinic <filip.marinic@vsmti.hr>
2018-11-08 19:01:09 +0100

    PMA

--- app/src/main/java/com/example/student/myapplication/StudentInfoActivity.java
+++ app/src/main/java/com/example/student/myapplication/StudentInfoActivity.java
@@ -1,7 +1,11 @@
 package com.example.student.myapplication;
 
+import android.content.Intent;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StudentInfoActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class StudentInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_info);
+
+        TextInputLayout Inpt2 = (TextInputLayout)findViewById(R.id.textInputLayoutID);
+        Button btn = (Button)findViewById(R.id.buttonID);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent2 = new Intent(StudentInfoActivity.this, SummaryActivity.class);
+                myIntent2.putExtra("predmet", "Inpt2"); //Optional parameters
+                StudentInfoActivity.this.startActivity(myIntent2);
+            }
+        });
     }
 }
